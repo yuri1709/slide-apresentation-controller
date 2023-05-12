808ba4ec7f59f3c32d486289df8743fdc38ebdd7
yuri1709 <yuriconder184@gmail.com>
2023-05-12 13:23:31 -0300

    Removido : localtunner

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import { exec } from "child_process";
-// import localtunnel from './node_modules/localtunnel/localtunnel.js'
 import path from 'path';
 import { keyboard } from "@nut-tree/nut-js";
 import { Key } from "@nut-tree/nut-js/dist/lib/key.enum.js";
@@ -9,7 +8,7 @@ import ngrok from 'ngrok';
 
 //Configurações globais
 const portValue = 3000;
-var subdomainValue = 'senac';
+var q= 'senac';
 const __dirname = path.resolve();
 var app = express();
 
@@ -42,12 +41,9 @@ app.get('/desligar', function(req, res) {
 })
 
 
-// const tunnel = await localtunnel({port: portValue, subdomain: subdomainValue});
 const url = await ngrok.connect(portValue);
-
 app.listen(portValue, function() {
-  //console.log('App listening on port ',portValue);
-  // console.log('App generated url: ',tunnel.url);
+  console.log('App listening on port ',portValue);
   console.log(url)
 })
 
